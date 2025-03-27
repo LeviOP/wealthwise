9d0be211a4bf8f45bfcb0be4b6bcc056fd133860
LeviOP <l3viop@gmail.com>
2025-03-27 17:21:17 -0500

    fix endpoint of graphql

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: import.meta.env.DEV ? 'http://localhost:4000/graphql' : '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
